a0da8e4ed777d50c8c924cf4abca534ea9164cf4
Azahar Hossain <azahar.hossain10@yahoo.com>
2020-12-03 06:29:36 +0100

    Child routing of recipes component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { SelectRecipeComponent } from './recipes/select-recipe/select-recipe.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 const appRoutes: Routes  = [
     {path: '', redirectTo: '/recipes', pathMatch: 'full'},
-    {path: 'recipes', component: RecipesComponent},
+    {path: 'recipes', component: RecipesComponent, children: [
+        {path: '', component: SelectRecipeComponent},
+        {path : ':id', component: RecipeDetailComponent},
+        {path : ':new', component: RecipeEditComponent},
+        {path: ':id/edit', component: RecipeEditComponent}
+    ]},
     {path: 'shopping-list', component: ShoppingListComponent}
 ];
 @NgModule({
