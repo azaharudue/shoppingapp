44ebec42c3a2eee8acadda353c5f4018e80d906f
Azahar Hossain <azahar.hossain@ampada.local>
2020-12-08 10:02:24 +0100

    217: Added TD in form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,9 +1,5 @@
-import {
-  Component,
-  OnInit,
-  ElementRef,
-  ViewChild
-} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shoppingList.service';
@@ -11,22 +7,16 @@ import { ShoppingListService } from '../shoppingList.service';
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
-  @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
+  constructor(private slService: ShoppingListService) {}
 
-  constructor(private slService: ShoppingListService) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
-  onAddItem(): void {
-    const ingName = this.nameInputRef.nativeElement.value;
-    const ingAmount = this.amountInputRef.nativeElement.value;
-    const newIngredient = new Ingredient(ingName, ingAmount);
+  onAddItem(form: NgForm): void {
+    const value = form.value;
+    const newIngredient = new Ingredient(value.name, value.amount);
     this.slService.addIngredient(newIngredient);
   }
-
 }
